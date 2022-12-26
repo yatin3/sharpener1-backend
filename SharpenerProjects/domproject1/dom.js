a7e579411aa83f9555f854d31a5df3ae57a98568
yatin <yatingoyal31@gmail.com>
2022-12-26 15:04:35 +0530

    second commi

---
+++ SharpenerProjects/domproject1/dom.js
@@ -0,0 +1,34 @@
+//console.dir(document);
+
+// console.log(document.domain);
+// console.log(document.url);
+
+// console.log(document.title);
+
+// document.title = "hello";
+// console.log(document.body);
+// console.log(document.head);
+
+// console.log(document.all);
+// console.log(document.all[10]);
+
+// document.all[10]textContent = "hello";
+
+// console.log(document.getElementById('header-title'));
+
+// var headerTitle = document.getElementById('header-title');
+// headerTitle.textContent = 'hello';
+
+// headerTitle.innerhtml = '<h3>hello</h3>';
+
+// var header = document.getElementById('main-header');
+
+// header.style.borderBottom = 'solid 3px black';
+
+var title = document.getElementsByClassName('title')[0];
+
+
+title.style.color = 'green';
+title.style.fontWeight = 'bold';
+
+console.log(title);
