f4834e24a6bb3e1e646ac64cf49cba1e4dc22ede
yatin <yatingoyal31@gmail.com>
2023-01-13 13:21:31 +0530

    used fs and node

---
+++ first-app.js
@@ -0,0 +1,7 @@
+console.log("hello world");
+
+// const fs = require("fs");
+// //fs.writeFileSync('C:/Users/yatingoyal/Documents/SharpenerProjects/backend/hello.txt',"hello from node.js");
+
+// fs.writeFileSync('gt/hello1.txt',"hello from node.js");
+// fs.writeFileSync('hello.txt',"hello from node.js");
