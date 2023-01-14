cea1a28957d6bd13e5fc26d6743b2ba6557883e0
yatin <yatingoyal31@gmail.com>
2023-01-14 15:02:07 +0530

    response got

--- server.js
+++ server.js
@@ -1,12 +1,24 @@
 
 const http = require('http');
 
-function rqListener(req,res){
-
-}
-
 const server = http.createServer((req,res)=>{
-    console.log(req);
+    // console.log(req.headers ,req.url ,req.method);
+    // process.exit();
+
+     res.setHeader('content-Type','text/html');
+     res.write('<html>');
+     res.write('<head><title>My First Page</title></head>');
+     if(req.url==='/home'){
+        res.write('<body><h1>welcome home</h1></body>');
+     }
+     else if(req.url==='/about'){
+        res.write('<body><h1>Welcome to About Us page</h1></body>');
+     }
+     else if(req.url==='/node'){
+        res.write('<body><h1>Welcome to my Node Js project</h1></body>');
+     }
+     res.write('</html>');
+     res.end();
 });
 
 server.listen(4000);
