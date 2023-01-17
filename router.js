b7b81ab4f57193d63b106315a420793403acc91e
yatin <yatingoyal31@gmail.com>
2023-01-17 14:34:14 +0530

    router and filter in express

--- router.js
+++ router.js
@@ -3,26 +3,41 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+//added feature of routing
+//const adminRoutes = require('C:/Users/yatingoyal/Documents/SharpenerProjects/backend/Express project/routes/admin.js');//absolute path
+
+const adminRoutes = require('./routes/admin');//relative path
+const shopRoutes = require('./routes/shop');
+
+
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.use('/',(req,res,next)=>{ 
-    console.log("This Will Always Run");
-    next();
+//added feature of routing
+app.use('/admin',adminRoutes);
+app.use('/shop',shopRoutes);
+
+app.use((req,res,next)=>{ 
+   res.status(404).send('<h1>Page not found</h1>');
 });
 
-app.use('/add-product',(req,res,next)=>{ 
-    console.log("In Another Middleware!");
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">Add Product</button></form>');
-});
+// app.use('/',(req,res,next)=>{ 
+//     console.log("This Will Always Run");
+//     next();
+// });
 
-app.use('/product',(req,res,next)=>{
-    console.log(req.body);
-    res.redirect('/');
-});
+// app.use('/add-product',(req,res,next)=>{ 
+//     console.log("In Another Middleware!");
+//     res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">Add Product</button></form>');
+// });
 
-app.use('/',(req,res,next)=>{ 
-     console.log("In Another Middleware!");
-     res.send('<h1>Hello from express!</h1>');
- });
+// app.post('/product',(req,res,next)=>{
+//     console.log(req.body);
+//     res.redirect('/');
+// });
+
+// app.use('/',(req,res,next)=>{ 
+//      console.log("In Another Middleware!");
+//      res.send('<h1>Hello from express!</h1>');
+//  });
  
  app.listen(3000);
---
+++ routes/shop.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/',(req,res,next)=>{ 
+    console.log("In Another Middleware!");
+    res.send('<h1>Hello from express!</h1>');
+});
+
+
+module.exports = router;
